5ee4e5f09b27bd338c7c038c796f21af57d4ebcc
Anastasiia <felitsias22@gmail.com>
2025-05-04 22:08:23 +0200

    Updated Header component style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
 import { Link } from "react-router-dom";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import socials from "../constants/socials";
 
 const Header = () => {
     return (
-        <Navbar collapseOnSelect fixed="top" expand="sm" data-bs-theme="dark">
+        <Navbar 
+            collapseOnSelect 
+            fixed="top" 
+            expand="sm" 
+            data-bs-theme="dark"
+            className="py-2 px-4 bg-navbar border"
+        >
             <Container fluid>
-                <Navbar.Brand>Portfolio</Navbar.Brand>
+                <Navbar.Brand className="fs-3 fw-bold text-accent">Portfolio</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto" defaultActiveKey="/portfolio/">
+                <Navbar.Collapse 
+                    id="responsive-navbar-nav"
+                    className="justify-content-end me-4 fs-5"
+                >
+                    <Nav defaultActiveKey="/portfolio/">
                         {[
                             { label: 'Home', to: '/portfolio/' },
                             { label: 'Projects', to: '/portfolio/projects' }
@@ -28,8 +36,9 @@ const Header = () => {
                                     href={url}
                                     target="_blank"
                                     rel="noopener noreferrer"
+                                    className="d-flex justify-content-center py-2 social-item"
                                 >
-                                    <FontAwesomeIcon icon={icon} size="2x" />
+                                    <FontAwesomeIcon icon={icon} size="2x" className="social-icon"/>
                                 </NavDropdown.Item>
                             ))}
                         </NavDropdown>
