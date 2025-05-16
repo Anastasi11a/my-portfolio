d6c06453c3535f39c675fe2ec6f4501adbee3e8e
Anastasiia <felitsias22@gmail.com>
2025-05-16 15:05:42 +0200

    Updated App.css

--- src/utils/classNames.js
+++ src/utils/classNames.js
@@ -1,34 +1,34 @@
 // Header
-export const navbar = 'py-2 px-4 bg-navbar border-navbar shadow-sm';
+export const navbar = 'py-2 px-4 shadow-sm bg-primary border-navbar';
 export const brand = 'fs-3 fw-bold text-accent';
 export const navCollapse = 'justify-content-end me-4 fs-5';
 export const socialItem = 'd-flex justify-content-center py-2 social-item';
 
 // Footer
-export const footerContainer = 'w-100 p-3 bg-footer border-top border-black';
+export const footerContainer = 'w-100 p-3 border-top border-black bg-secondary';
 export const rowCenter = 'justify-content-center';
 export const footerText = 'text-center text-uppercase text-light fs-6 mb-2 mt-2 pb-3 footer-text';
 
 // Home
-export const homeContainer = 'min-vh-100 d-flex flex-column justify-content-center align-items-center pt-5 bg-home';
+export const homeContainer = 'min-vh-100 d-flex flex-column justify-content-center align-items-center pt-5 bg-primary';
 export const centeredRow = 'align-items-center justify-content-center';
 export const imageWrapper = 'text-center mb-4';
 export const image = 'mx-auto d-block avatar-img';
-export const greeting = 'mb-3 fs-3 text-dark-text';
-export const role = 'mb-2 fs-3 fw-bold text-uppercase text-accent animated-fadeInUp';
-export const tagline = 'mb-3 fs-4 fw-bold text-dark-text animated-fadeInUp';
+export const greeting = 'mb-3 fs-3 text-light';
+export const role = 'mb-2 fs-3 fw-bold text-uppercase animated-fadeInUp text-accent';
+export const tagline = 'mb-3 fs-4 fw-bold text-light animated-fadeInUp';
 
 // Projects
 export const section = 'bg-dark py-5';
 export const projectsContainer = 'min-vh-100 d-flex flex-column justify-content-center align-items-center pt-5';
 
 // ProjectCard
-export const card = 'h-100 text-light border-0 shadow-sm bg-card';
+export const card = 'h-100 text-light border-0 shadow-sm bg-secondary';
 export const cardImg = 'd-block mx-auto mb-1 rounded card-img';
 export const cardTitle = 'text-center fw-bold fs-3 mb-3 card-title';
 export const cardText = 'text-start text-light fs-6 mb-2';
 export const skillsText = 'fw-bold fs-5 ps-2';
-export const cardBtn = 'p-0 mb-1 text-decoration-none skills-text collapse-item';
+export const cardBtn = 'p-0 mb-1 text-decoration-none collapse-item features-text';
 
 // ButtonLink
 export const githubLink = "d-flex justify-content-end mb-3 pe-3"
